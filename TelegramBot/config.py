59d5b0d0114305ebd8f0f53261f4bed88e60c74b
vva07 <vva0779@yandex.ru>
2024-06-10 03:45:31 +0300

    refactor(): tweak docker

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "7105595371:AAF3t8o4ltEtMhg2M0eYiCtTwGDsIaU5Yv4"
 INTERNAL_API_TOKEN = "qmki-01S8qc5eUE1RCoFYw65L7bSj7zvMJlYeuSFLmw"
-API_BASE_URL = "http://api:8000"
+API_BASE_URL = "http://api:5000"
 BOT_URL = "https://t.me/homeresource_bot"
